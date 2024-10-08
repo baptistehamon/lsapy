229c93b4c5e5b4c1503efc773976b5d9789300ea
Baptiste Hamon <baptiste.hamon@pg.canterbury.ac.nz>
2024-10-08 15:44:05 +1300

    Add suitability functions

--- functions.py
+++ functions.py
@@ -1,36 +1,204 @@
 "Suitability function definitions"
 
-import numpy as np
-import scipy.stats as stats
-
-class SuitabilityFunction:
-    def __init__(self, name, params):
-        pass
-
-def sigmoid():
-    pass
-
-def asymsigmoid(x, c, d, m):
-    return 1 / (np.power(1 + np.power(x/c, d), m))
+from typing import Optional, Callable
 
 import matplotlib.pyplot as plt
-from scipy.stats import genlogistic
+import numpy as np
 from scipy.optimize import curve_fit
 
+# from ...functions import logistic...
 
-indicator_vals = np.array([700,750,800,850,1000])
-suit_vals = np.array([0,0.25,0.5,0.75,1])
+class SuitabilityFunction:
 
-x = np.linspace(0, 1200, 100)
-y_asymsigmoid = asymsigmoid(x, 501, -13.1, 296)
+    def __init__(
+            self,
+            func: Optional[Callable] = None,
+            func_name: Optional[str] = None,
+            func_params: Optional[dict[str, int | float]] = None
+    ):
+        if func is not None or func_name is not None:
+            if func_params is None:
+                raise ValueError("If 'func' or 'func_name' is provided, 'func_params' must also be provided.")
+        
+        if func_params is not None:
+            if func is None and func_name is None:
+                raise ValueError("If 'func_params' is provided, 'func' or 'func_name' must also be provided.")
 
-popt, _ = curve_fit(genlogistic.cdf, indicator_vals, suit_vals, p0=[1, np.median(x),1], maxfev=15000)
-y_genlogistic = genlogistic.cdf(x, *popt)
+        self.func = func
+        self.func_name = func_name
+        self.func_params = func_params
+        if func is None and func_name is not None:
+            self.func = _get_function_from_name(func_name)
+        
+    def __call__(self, x):
+        if self.func is None:
+            raise ValueError("No function has been provided.")
+        return self.func(x, **self.func_params)
+    
+    def map(self, x):
+        return self.__call__(x)
 
- = genlogistic.fit(indicator_vals)
+# ---------------------------------------------------------------------------- #
+# ------------------------ Membership functions ------------------------------ #
+# ---------------------------------------------------------------------------- #
+_MEMBERSHIP_FUNCTIONS = [
+    'logistic',
+    'vetharaniam2022_eq3',
+    'vetharaniam2022_eq5'
+]
 
-plt.plot(x, y_asymsigmoid, label='Asymmetric Sigmoid')
-plt.plot(x, y_genlogistic, label='Generalized Logistic')
-plt.scatter(indicator_vals, suit_vals, c='r')
-plt.legend()
-plt.show()
+class MembershipSuitFunction(SuitabilityFunction):
+    _methods = _MEMBERSHIP_FUNCTIONS
+
+    def __init__(
+            self,
+            func: Optional[Callable] = None,
+            func_name: Optional[str] = None,
+            func_params: Optional[dict[str, int | float]] = None
+    ):
+        super().__init__(func, func_name, func_params)
+
+
+    @staticmethod
+    def fit(x, y = np.array([0,.25,.5,.75,1]), methods: str | list[str] = 'all', plot: bool = False):
+        return _fit_mbs_functions(x, y, methods, plot)
+
+
+def logistic(x, a, b):
+    return 1 / (1 + np.exp(-a*(x - b)))
+
+def sigmoid(x):
+    return logistic(x, 1, 0)
+
+def logistic_vetharaniam2022_eq3(x, a, b):
+    return np.exp(a * (x - b)) / (1 + np.exp(a * (x - b)))
+
+def logistic_vetharaniam2022_eq5(x, a, b):
+    return 1 / (1 + np.exp(a * (np.sqrt(x) - np.sqrt(b))))
+
+
+# TODO: Check if a general logistic working with positive and negative values exists
+# def _general_logistic(x, c, d, m):
+#     return 1 / (np.power(1 + np.power(x/c, d), m))
+
+# TODO: Add a general normal distribution function
+
+
+def _get_mbs_function_from_name(name: str) -> Callable:
+    if name.lower() not in _MEMBERSHIP_FUNCTIONS:
+        raise ValueError(f"Method must be one of {_MEMBERSHIP_FUNCTIONS}. Got {name}")
+
+    if name.lower() == 'logistic':
+        return logistic
+    elif name.lower() == 'vetharaniam2022_eq3':
+        return logistic_vetharaniam2022_eq3
+    elif name.lower() == 'vetharaniam2022_eq5':
+        return logistic_vetharaniam2022_eq5
+
+
+def _fit_mbs_functions(x, y, methods: str | list[str] = 'all', plot: bool = False):
+    if methods == 'all':
+        methods = _MEMBERSHIP_FUNCTIONS
+    elif isinstance(methods, str):
+        methods = [methods]
+    else:
+        raise ValueError(f"'methods' must be a string or a list of strings. Got {methods}")
+    
+    x_ = np.linspace(min(x), max(x), 100)
+    rms_errors = []
+    f_params = []
+    for method in methods:
+        f = _get_mbs_function_from_name(method)
+        popt, _ = curve_fit(f, x, y, p0=[1, np.median(x)], maxfev=15000)
+        y_ = f(x_, *popt)
+        f_params.append(popt)
+        rmse = _rms_error(y,f(x, *popt))
+        rms_errors.append(rmse)
+        if plot:
+            plt.plot(x_, y_, label=method + f' (RMSE={rmse:.2f})')
+    if plot:
+        plt.scatter(x, y, c='r')
+        plt.legend()
+        plt.show()
+    
+    f_best, p_best = _get_best_fit(methods, rms_errors, f_params)
+    return _get_mbs_function_from_name(f_best), p_best
+
+
+# ---------------------------------------------------------------------------- #
+# --------------------------- Discrete functions ----------------------------- #
+# ---------------------------------------------------------------------------- #
+_DISCRETE_FUNCTIONS = [
+    'discrete'
+]
+
+class DiscreteSuitFunction(SuitabilityFunction):
+    _methods = _DISCRETE_FUNCTIONS
+
+    def __init__(
+            self,
+            func: Optional[Callable] = None,
+            func_name: Optional[str] = None,
+            func_params: Optional[dict[str, int | float]] = None
+    ):
+        super().__init__(func, func_name, func_params)
+
+
+def discrete(rules: dict, x: int) -> float:
+    """
+    Returns a function that maps keys to values in a given dictionary.
+
+    Args:
+        dict_ruleset (dict): A dictionary containing the mapping rules.
+
+    Returns:
+        function: A function that takes a key as an argument and returns the corresponding value
+        from the dictionary, or None if the key is not found.
+
+    Examples:
+        >>> rules = {'a': 1, 'b': 2, 'c': 3}
+        >>> map_func = discrete(rules)
+        >>> map_func('b')
+        2
+        >>> map_func('d')
+        None
+    """
+    return lambda x: rules.get(x)
+
+def _get_discrete_function_from_name(name: str) -> Callable:
+    if name.lower() not in _DISCRETE_FUNCTIONS:
+        raise ValueError(f"Method must be one of {_DISCRETE_FUNCTIONS}. Got {name}")
+
+    if name.lower() == 'discrete':
+        return discrete
+
+# ---------------------------------------------------------------------------- #
+# ---------------------------- Utility functions ----------------------------- #
+# ---------------------------------------------------------------------------- #
+_FUNCTIONS = _MEMBERSHIP_FUNCTIONS + _DISCRETE_FUNCTIONS
+
+def _get_function_from_name(name: str):
+    if name.lower() not in _FUNCTIONS:
+        raise ValueError(f"Method must be one of {_FUNCTIONS}. Got {name}")
+    
+    if name.lower() in _MEMBERSHIP_FUNCTIONS:
+        return _get_mbs_function_from_name(name)
+    elif name.lower() in _DISCRETE_FUNCTIONS:
+        return _get_discrete_function_from_name(name)
+
+
+def _rms_error(y_true, y_pred):
+    diff = abs(y_true - y_pred)
+    return np.sqrt(np.mean(diff**2))
+
+
+def _get_best_fit(methods, rmse, params, verbose=True):
+    best_fit = np.argmin(rmse)
+    if verbose:
+        print(f"""
+Best fit: {methods[best_fit]}
+RMSE: {rmse[best_fit]:.5f}
+Params: a={params[best_fit][0]}, b={params[best_fit][1]}
+""")
+    return methods[best_fit], params[best_fit]
+
--- landsuitability.py
+++ landsuitability.py
@@ -30,19 +30,19 @@ class SuitabilityCriteria:
         pass
 
 
-class SuitabilityFunctions:
-    
-    def __init__(self) -> None:
-        pass
 
+from landsuitability.testing.utils import load_data
+from landsuitability.functions import SuitabilityFunction, MembershipSuitFunction, DiscreteSuitFunction
+import numpy as np
+import matplotlib.pyplot as plt
 
-class MembershipSuitFunctions:
-    
-    def __init__(self) -> None:
-        pass
+SuitabilityFunction(func_name='logistic', func_params={'a': 1, 'b': 2})
 
+MembershipSuitFunction.fit(x=[1500,1350,1250,1150,1000], plot=True)
 
-class DiscreteSuitFunctions:
-    
-    def __init__(self) -> None:
-        pass
+f_suit = SuitabilityFunction(func_name='vetharaniam2022_eq5', func_params={'a': 0.8759, 'b': 1248.5})
+
+x = np.linspace(500, 2000, 100)
+y = f_suit(x)
+plt.plot(x, y)
+plt.show()

34fdd134189f83b0273da169ac4b38417ceaff28
Baptiste Hamon <baptiste.hamon@pg.canterbury.ac.nz>
2025-03-12 19:28:16 +1300

    First release cleaning 2

--- src/lsapy/__init__.py
+++ src/lsapy/__init__.py
@@ -4,6 +4,6 @@ from lsapy.lsa import LandSuitability
 from lsapy.criteria import SuitabilityCriteria
 from lsapy.functions import *
 
-__author__ = """Baptiste Hamon"""
-__email__ = """baptiste.hamon@pg.canterbury.ac.nz"""
-__version__ = """0.0.0"""
+__author__ = "Baptiste Hamon"
+__email__ = "baptiste.hamon@pg.canterbury.ac.nz"
+__version__ = "0.0.0"

923534e032c80cf65e7bd625334882cdc73ec7fb
Baptiste Hamon <baptiste.hamon@pg.canterbury.ac.nz>
2025-05-04 15:30:33 +0200

    Update version and release in conf file

--- docs/conf.py
+++ docs/conf.py
@@ -6,16 +6,20 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../src/'))
+sys.path.insert(0, os.path.abspath("../src/"))
 
 import lsapy
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'lsapy'
-copyright = '2025, Baptiste Hamon'
-author = 'Baptiste Hamon'
+project = "lsapy"
+copyright = "2025, Baptiste Hamon"
+author = "Baptiste Hamon"
+
+# The short X.Y version.
+version = lsapy.__version__.split("-")[0]
+# The full version, including alpha/beta/rc tags.
 release = lsapy.__version__
 
 # -- General configuration ---------------------------------------------------
@@ -31,7 +35,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_title = 'lsapy Documentation'
+html_title = 'lsapy'
 html_short_title = 'lsapy'
 
 html_theme = 'pydata_sphinx_theme'
